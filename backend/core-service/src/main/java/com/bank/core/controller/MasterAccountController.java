f9a8fd3eb2a44e0ca14e8c29fa005a4debf3329a
VLsoft-eng <deeeaddrop@gmail.com>
2026-03-20 01:44:28 +0700

    fix: fix master account endpoint

--- backend/core-service/src/main/java/com/bank/core/controller/MasterAccountController.java
+++ backend/core-service/src/main/java/com/bank/core/controller/MasterAccountController.java
@@ -1,9 +1,14 @@
 package com.bank.core.controller;
 
 import com.bank.core.dto.AccountResponse;
+import com.bank.core.dto.MasterAccountTransferRequest;
+import com.bank.core.dto.TransferRequest;
 import com.bank.core.model.Currency;
 import com.bank.core.service.AccountService;
+import com.bank.core.service.OperationService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +19,7 @@ import java.util.List;
 public class MasterAccountController {
 
     private final AccountService accountService;
+    private final OperationService operationService;
 
     @GetMapping
     public List<AccountResponse> getMasterAccounts() {
@@ -23,5 +29,18 @@ public class MasterAccountController {
     @GetMapping(params = "currency")
     public AccountResponse getMasterAccount(@RequestParam Currency currency) {
         return accountService.getMasterAccount(currency);
+    }
+
+    @PostMapping("/transfer")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void transferFromMasterAccount(@Valid @RequestBody MasterAccountTransferRequest request) {
+        AccountResponse targetAccount = accountService.getAccountById(request.targetAccountId());
+        AccountResponse masterAccount = accountService.getMasterAccount(targetAccount.currency());
+
+        operationService.requestTransfer(new TransferRequest(
+                masterAccount.id(),
+                request.targetAccountId(),
+                request.amount()
+        ));
     }
 }
